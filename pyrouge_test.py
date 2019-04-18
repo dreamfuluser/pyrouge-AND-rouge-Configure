9a77b64fa2e1773b6c2f2af3de3194cdc9c7335f
dreamfuluser <44727590+dreamfuluser@users.noreply.github.com>
2019-04-18 15:47:48 +0800

    Update pyrouge_test.py

--- pyrouge_test.py
+++ pyrouge_test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # -------------------------------------------------
-# Author: Amy_Zhang
-# DateTime: 2018/11/29 21:32
+# Author: lv rui
+# DateTime: 2019/4/17 21:32
 # -------------------------------------------------
 import pyrouge
 import logging
@@ -10,8 +10,8 @@ import logging
 r = pyrouge.Rouge155()
 r.model_filename_pattern = 'ref.#ID#.txt'
 r.system_filename_pattern = 'cand.(\d+).txt'
-r.model_dir = 'D:/untitled/BertSum-master/results/model'
-r.system_dir = 'D:/untitled/BertSum-master/results/system'
+r.model_dir = 'D:/workspace/text-summarization-model/results/model'
+r.system_dir = 'D:/workspace/text-summarization-model/results/system'
 # r.model_dir = '../log/decode_data/reference'
 # r.system_dir = '../log/decode_data/decoded'
 # logging.getLogger('global').setLevel(logging.WARNING)  # silence pyrouge logging
